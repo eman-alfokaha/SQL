9d61f97f01337590189c7049a5d50625c820ca20
eman-alfokaha <115453404+eman-alfokaha@users.noreply.github.com>
2022-12-12 16:38:54 +0300

    Add files via upload

---
+++ 12-12-2022/SQLQuery1.sql
@@ -0,0 +1,40 @@
+--task1
+create table customers
+(
+	ID int  not null Primary Key,
+	name varchar(50),
+	Email varchar(50),
+
+)
+
+--task2
+
+
+create table orders
+(
+	ID int  not null Primary Key,
+	name varchar(50),
+	Email varchar(50),
+
+)
+
+--task3
+create table Products
+(
+	ID int  not null Primary Key,
+	name varchar(50),
+	Email varchar(50),
+
+)
+
+--task4
+create table Employees
+(
+	ID int  not null Primary Key,
+	name varchar(50),
+	Email varchar(50),
+
+)
+
+
+
